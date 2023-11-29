1397ae154ce7df509cd2ff707d2398203f460a2c
jvillacinda <jvillacoding@gmail.com>
2023-11-28 20:41:34 -0800

    Functional. Removed functions.cpp

--- final/final.ino
+++ final/final.ino
@@ -1,17 +1,7 @@
-#include "functions.cpp"
 #include "globals.h"
 
-
 void setup() {
-  pinMode(PWMA, OUTPUT);     
-  pinMode(PWMB, OUTPUT);
-  pinMode(BIN, OUTPUT);
-  pinMode(AIN, OUTPUT);
-  pinMode(STBY, OUTPUT);
-  digitalWrite(STBY, HIGH);  //Enable Motors to run
-
-  digitalWrite(PWMA, LOW);  // Fully on 
- // digitalWrite(PWMA, HIGH);  // Fully on
+Device_Setup();
 }
 
 //main loop
@@ -23,7 +13,108 @@ void loop() {
 
   delay(2000);
 
- //starts robot
 S_Path();
 
 }
+
+
+
+
+//functions
+void Device_Setup()
+{
+    pinMode(PWMA, OUTPUT);     
+  pinMode(PWMB, OUTPUT);
+  pinMode(BIN, OUTPUT);
+  pinMode(AIN, OUTPUT);
+  pinMode(STBY, OUTPUT);
+  digitalWrite(STBY, HIGH);  //Enable Motors to run
+
+  digitalWrite(PWMA, LOW);  // Fully on 
+ // digitalWrite(PWMA, HIGH);  // Fully on
+}
+
+// Function - accepts the time in milli-Seconds to go into standby for
+void stopTime(int mS){
+  digitalWrite(STBY, LOW);  
+  delay(mS);                
+  digitalWrite(STBY, HIGH);  
+}
+
+void Straight()
+{
+  analogWrite(STBY, MaxSpeed);
+  digitalWrite(AIN, HIGH);    
+  digitalWrite(BIN, HIGH);    
+  digitalWrite(PWMA, HIGH);   
+  digitalWrite(PWMB, HIGH);   
+  delay(driveTime);              
+  stopTime(breakTime);
+}
+
+void Right()
+{
+  analogWrite(STBY, MaxSpeed);
+  digitalWrite(AIN, LOW);    
+  digitalWrite(BIN, HIGH);
+  digitalWrite(PWMA, HIGH);   
+  digitalWrite(PWMB, HIGH);
+  delay(turnTime);    
+  stopTime(breakTime);
+}
+
+void Left()
+{
+   analogWrite(STBY, MaxSpeed);
+  digitalWrite(AIN, HIGH);    
+  digitalWrite(BIN, LOW);
+  digitalWrite(PWMA, HIGH);   
+  digitalWrite(PWMB, HIGH);
+  delay(turnTime); 
+  stopTime(breakTime);
+}
+
+void uTurn()
+{
+  analogWrite(STBY, MaxSpeed);
+  digitalWrite(AIN, HIGH);    
+  digitalWrite(BIN, LOW);
+  digitalWrite(PWMA, HIGH);   
+  digitalWrite(PWMB, HIGH);
+  delay(turnTime);              
+  stopTime(breakTime);
+}
+
+void Stop()
+{
+  digitalWrite(AIN, LOW);    
+  digitalWrite(BIN, LOW);    
+  digitalWrite(PWMA, LOW);   
+  digitalWrite(PWMB, LOW);   
+  delay(driveTime);    
+}
+
+void S_Path()
+{
+  Straight();
+  Left();  
+  Straight();
+  Left();  
+  Straight();
+  Right(); 
+  Straight();
+  Right(); 
+  Straight();
+  uTurn(); 
+  Straight();
+  Left();  
+  Straight();
+  Left();  
+  Straight();
+  Right();  
+  Straight();
+  Right();
+  Straight();
+  Stop();
+}
+
--- final/globals.h
+++ final/globals.h
@@ -1,19 +1,28 @@
 #ifndef _globals_H_
 #define _globals_H_
 
-#include <arduino.h>
+//function prototypes
+void Device_Setup();
+void stopTime(int mS); // Pauses car in between motions
+void Straight(); // Moves Car Straight
+void Right(); // Moves Car Right
+void Left(); // Moves Car Left
+void uTurn(); // Makes car perform a u-turn
+void Stop(); // Stops car
 
+
+//device information
 #define PWMA 5    
 #define PWMB 6    
 #define AIN 7     
 #define BIN 8     
 #define STBY 3    
-#define MaxSpeed 100
+#define MaxSpeed 100 // Maximum Speed of the Car
 #define modeSwitch 2 // Mode Switch input
 
 //Change variable time here
-#define driveTime 4000
-#define turnTime 650
-#define uTurnTime 1300
-#define breakTime 1000
+#define driveTime 3000 // duration the car drives straight
+#define turnTime 630 // Duration the car takes on left and right turns 
+#define uTurnTime 1275 // Duration the car takes to make a u-turn
+#define breakTime 2000 // Pause in between motions
 #endif
